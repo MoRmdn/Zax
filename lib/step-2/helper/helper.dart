28f0b1a2ca6a0d68d7710fff17ac9f5966cb9f28
Mohamed Ramadan <MoRm9n@gmail.com>
2022-10-16 11:40:32 +0200

    Flutter Notes

--- lib/step-2/helper/helper.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:math' as math show Random;
-
-//* add this method to Iterable Class
-extension GetRandomValue<T> on Iterable<T> {
-  T getRandomValue() => elementAt(math.Random().nextInt(length));
-}
